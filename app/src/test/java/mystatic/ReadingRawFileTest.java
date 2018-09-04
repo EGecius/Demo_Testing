7d4c3230172ed8bb7e6b2e81b227861920350f0f
Egidijus Gecius <>
2018-09-04 15:11:00 +0100

    demo how read content from json

---
+++ app/src/test/java/mystatic/ReadingRawFileTest.java
@@ -0,0 +1,31 @@
+package mystatic;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class ReadingRawFileTest {
+
+
+    private static final String EXPECTED_JSON_CONTENT = "{\"name\": \"Egis\"}";
+
+    @Test
+    public void readJsonFile() {
+        String resourceAsString = getResourceAsString("/remote_config.json");
+        assertThat(resourceAsString).isEqualTo(EXPECTED_JSON_CONTENT);
+    }
+
+
+    private String getResourceAsString(String resource) {
+        try {
+            return IOUtils.toString(this.getClass().getResourceAsStream(resource), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        throw new RuntimeException("error reading file");
+    }
+}
